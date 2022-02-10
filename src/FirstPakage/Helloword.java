838d977761a7b768047ed5a1a1118668d7ac3f82
nmayoba <nmayoba@gmail.com>
2022-02-10 18:49:18 +0200

    Hello world this is Precious

---
+++ src/FirstPakage/Helloword.java
@@ -0,0 +1,10 @@
+package FirstPakage;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello word my name is Precious");
+	}
+
+}
